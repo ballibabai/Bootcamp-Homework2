6a89ec61249ebd8804af484e7fd1899fa72bdf0a
İbrahim Ballıbaba <w4nderlust3rr@gmail.com>
2022-09-14 17:36:14 +0300

    init
    First init

---
+++ NewsApp/NewsApp/MainCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MainCollectionViewCell.swift
+//  NewsApp
+//
+//  Created by İbrahim Ballıbaba on 14.09.2022.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var newsImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+}
---
+++ NewsApp/NewsApp/News.swift
@@ -0,0 +1,8 @@
+//
+//  News.swift
+//  NewsApp
+//
+//  Created by İbrahim Ballıbaba on 14.09.2022.
+//
+
+import Foundation
