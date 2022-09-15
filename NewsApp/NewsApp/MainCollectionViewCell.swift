58c24613f23d3f157263a3763c87d48956451d96
İbrahim Ballıbaba <w4nderlust3rr@gmail.com>
2022-09-15 13:52:53 +0300

    Pages
    Added Pages

---
+++ NewsApp/NewsApp/MainCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MainCollectionViewCell.swift
+//  NewsApp
+//
+//  Created by İbrahim Ballıbaba on 15.09.2022.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ NewsApp/NewsApp/News.swift
@@ -0,0 +1,8 @@
+//
+//  News.swift
+//  NewsApp
+//
+//  Created by İbrahim Ballıbaba on 15.09.2022.
+//
+
+import Foundation
